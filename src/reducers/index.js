49d95ac43a223d62e46a8eaa2430f9f550c23376
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-05-17 14:41:42 +0300

    fix action.type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,23 @@
 
 const initialState = {
   books: [
-    {
-      id: 1,
-      title: "Site Reliability Engineering",
-      author: "Michael T. Nygard",
-      price: "34"
-    },
-    {
-      id: 2,
-      title: "Site Reliability Engineering",
-      author: "Michael T. Nygard",
-      price: "34"
-    }
+    // {
+    //   id: 1,
+    //   title: "Site Reliability Engineering",
+    //   author: "Michael T. Nygard",
+    //   price: "34"
+    // },
+    // {
+    //   id: 2,
+    //   title: "Site Reliability Engineering",
+    //   author: "Michael T. Nygard",
+    //   price: "34"
+    // }
   ]
 }
 
 const reducer = (state=initialState, action) => {
-  switch(action.tupe){
+  switch(action.type){
     case 'BOOKS_LOADED':
       return {
         books: action.payload
